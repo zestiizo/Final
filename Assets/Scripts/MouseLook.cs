07895fb7f89880fec4d171397bbb554b85002be0
Zoe <zmaddix00@gmail.com>
2019-11-25 13:17:39 -0600

    milestone 2 final
    done over the 11th week of game engine scripting

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    Vector2 mouseLook;
+    [SerializeField] float sensitivity;
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X");
+        float vertical = Input.GetAxis("Mouse Y");
+
+        Vector2 look = new Vector2(horizontal, vertical);
+        mouseLook += look * sensitivity;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -80f, 80);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.up);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+        transform.Translate(moveDirection);
+    }
+}
